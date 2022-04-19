effc13ecba5fa8141415d2d708aa8eefca15e3ad
Charlie <ciharland@hotmail.co.uk>
2022-04-19 11:33:52 +0000

    Install and use allauth login/out and signup features

--- joshblog/settings.py
+++ joshblog/settings.py
@@ -46,12 +46,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blosh',
 ]
+
+SITE_ID = 1
+# bringing the user back to homepage after login/out
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- joshblog/urls.py
+++ joshblog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blosh.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
