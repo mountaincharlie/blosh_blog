78c377be2f8dacc81571e23ab610277ecf9c1b85
Charlie <ciharland@hotmail.co.uk>
2022-04-19 09:47:33 +0000

    Move images dir from static dir to media dir

--- joshblog/settings.py
+++ joshblog/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join[BASE_DIR, 'staticfiles']
 
 MEDIA_URL = '/media/'
+# telling Django where our media files are
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
